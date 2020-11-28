03f7cab48874c2f135c73846a1f3dc24a4f3733d
KarimovRR <KarimovRR@ufanipi.ru>
2020-11-28 18:27:08 +0500

    Залил блокнот по импорту индекса промышленного производства

---
+++ data/regions.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+COUNTRY = "Российская Федерация"
+
+
+class Districts(Enum):
+    VOLGA = "Приволжский федеральный округ"
+    URAL = "Уральский федеральный округ"
+
+
+class Region(Enum):
+    """ Регионы """
+    Bash = "Республика Башкортостан"
+    Oren = "Оренбургская область"
+    Perm = "Пермский край"
+    Sverd = "Свердловская область"
+    Tymen = "Тюменская область"
+    Chel = "Челябинская область"
+
+
+# Названия регионов
+URAL_FILE_NAMES = {
+    Region.Bash: 'Республика_Башкортостан.csv',
+    Region.Oren: 'Оренбургская_область.csv',
+    Region.Perm: 'Пермский_край.csv',
+    Region.Sverd: 'Свердловcкая_область.csv',
+    Region.Tymen: 'Тюменская_область.csv',
+    Region.Chel: 'Челябинская_область.csv',
+}
